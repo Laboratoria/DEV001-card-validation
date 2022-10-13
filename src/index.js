d4a9ec5cdbac4b1cf0cbcba4c094d75f3deeea0c
Heberlis <heberlisfinol@gmail.com>
2022-10-13 16:12:11 -0300

    actualizacion card validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 import validator from './validator.js';
 
-console.log(validator);
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const creditCardNumber = (formulario["card-number"].value)
+  const verificar = validator.isValid(creditCardNumber);
+  alert(verificar);
+  const tarjeta = document.getElementById("card-number")
+  tarjeta.value = validator.maskify(creditCardNumber)
+
+
+
+  if(creditCardNumber==="") {
+ alert("Por favor llene todos los campos")
+  }
+})
+
+ // let creditCardNumber =document.getElementById("Tarjeta").value ;
+  
+
+
+  
+ 
+// if (creditCardNumber) {
+//    const valida= validator.isValid(creditCardNumber)
+//     if(valida===true){
+//    //document.getElementById("validMessage").style.display
+//    //document.getElementById("invalidMessage").style.display
+//    //document.getElementById("creditCardNumber").innerHTML
+//     }
+
+//   //---------Invocar Validator
+//   //validator.isValid(creditCardNumber);
+
+
+//   if (creditCardNumber === "") {
+//     alert("Debe llenar todos los campos")
+//   }
+// //else {
+// //document.getElementById("invalidMessage").style.display="grid";
+// //document.getElementById("validMessage").style.display="none";
+// //}
+//   //}
+// //else {
+// //alert("No es psoible validar")
+
+//   //}
+//   //mask.textContent = validator.maskify(creditCardNumber.value);
+// ///}}})
+
+
+
+
+
+//  // else {
+//     //Mostrando si la tarjeta es valida
+//    // alert(validator.isValid(creditCardNumber))
+
+//   //}
+
+  console.log(validator)
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,72 @@
 const validator = {
-  // ...
-};
+    isValid: function (creditCardNumber) {
+        let cardArr = creditCardNumber.split("").reverse();
+        // console.log({ cardArr });
+        for (let i = 0; i < cardArr.length; i++) {
+            if (i % 2 === 1) {
+                let acumulador = cardArr[i] * 2;
+                if (acumulador > 9) {
+                    cardArr[i] = acumulador.toString().split("").reduce((a, b) => parseInt(a) + parseInt(b), 0)
+                } else {
+                    cardArr[i] = acumulador;
+                }
+
+            }
+        }
+
+        let suma = 0;
+        for (let i = 0; i < cardArr.length; i++) {
+            suma += parseInt(cardArr[i]);
+        }
+
+        //console.log({ suma })
+        //console.log({ cardArr })
+
+        if (suma % 10 === 0) {
+          //  alert("tarjeta Valida");
+          return true;
+        }else { 
+            //alert("tarjeta invalida");
+            return false;
+        }
+
+
+        // if (i % 2 === 0) {
+
+
+        //     let acumulador = cardArr[i] * 2;
+        //     if (acumulador >= 10) {
+        //         let acumulador2 = acumulador - 9;
+        //         cardArr[i] = acumulador2;
+        //     } else {
+        //         cardArr[i] = acumulador;
+        //     }
+        // }
+
+        // let suma = 0;
+        // for (let i = 0; i < cardArr.length; i++) {
+        //     suma += parseInt(cardArr[i]);
+        // }
+
+        // console.log({suma});
+
+        // if (suma % 10 === 0) {
+        //     alert("tarjeta Valida");
+
+        // } else {
+        //     console.log("tarjeta invalida");
+        //     return false;
+        // }
+
+
+    },
+
+    maskify: function (creditCardNumber) {
+        const mask = creditCardNumber.length; 
+        return((""+ creditCardNumber).slice(0, mask -4).replace(/./g, "#") + (""+ creditCardNumber).slice( mask -4)
+        );
+    }
+
+}
 
 export default validator;
